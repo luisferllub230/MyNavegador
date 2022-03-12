e415eccb58e8621736883f06cba0156d557f1be0
ale <alejandroferllub@gmail.com>
2022-03-12 11:16:39 -0400

    type: opciones de busqueda

--- MyNavegador/Form1.cs
+++ MyNavegador/Form1.cs
@@ -10,11 +10,57 @@ using System.Windows.Forms;
 
 namespace MyNavegador
 {
-    public partial class Form1 : Form
+    public partial class MyNavegador : Form
     {
-        public Form1()
+        List<string> fav = new List<string>();
+
+        public MyNavegador()
         {
             InitializeComponent();
+        }
+
+        private void rButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void aButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void recargarButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void buscarButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(txtUrl.Text);
+        }
+
+        private void favoritosButton_Click(object sender, EventArgs e)
+        {
+            fav.Add(webBrowser1.Url.ToString());
+            addCombobox();
+        }
+
+        private void addCombobox() 
+        {
+            foreach (String i in fav) 
+            {
+                favoritosCB.Items.Add(i);
+            }
+        }
+
+        private void favoritosCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(favoritosCB.SelectedItem.ToString());
         }
     }
 }
